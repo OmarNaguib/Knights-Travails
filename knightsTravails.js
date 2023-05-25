426f13cca7d575ad71a24534e99b19bcf4b13eba
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-25 18:50:04 +0300

    Get knight moves

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,11 +1,21 @@
-function getNextPaths() {}
+const knightMoves = [
+  [1, 2],
+  [1, -2],
+  [-1, 2],
+  [-1, -2],
+  [2, 1],
+  [2, -1],
+  [-2, 1],
+  [-2, -1],
+];
+function getChildren() {}
 function knightsWay(initialPosition, targetPosition) {
   const queue = [[initialPosition]];
   while (queue.length > 1) {
     const currentPath = queue.pop();
     if (currentPath[currentPath.length - 1] === targetPosition)
       return currentPath;
-    queue.unshift(...getNextPaths(currentPath));
+    queue.unshift(...getChildren(currentPath));
   }
   return null;
 }
